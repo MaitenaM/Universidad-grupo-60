ee3a9e44a0645d1be4a19d1c229dcbd96010c99d
Maitena <maitenamoragues@outlook.es>
2023-09-08 20:11:56 -0300

    Initial commit

---
+++ src/universidad/grupo/pkg60/Entidades/Inscripcion.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidad.grupo.pkg60.Entidades;
+
+/**
+ *
+ * @author Maite
+ */
+public class Inscripcion {
+    
+    private int IDInscripcion;
+    private Alumno alumno;
+    private Materias materias;
+    private double nota;
+
+    public Inscripcion(int IDInscripcion, Alumno alumno, Materias materias, double nota) {
+        this.IDInscripcion = IDInscripcion;
+        this.alumno = alumno;
+        this.materias = materias;
+        this.nota = nota;
+    }
+
+    public Inscripcion(Alumno alumno, Materias materias, double nota) {
+        this.alumno = alumno;
+        this.materias = materias;
+        this.nota = nota;
+    }
+
+    public Inscripcion() {
+    }
+
+    public int getIDInscripcion() {
+        return IDInscripcion;
+    }
+
+    public void setIDInscripcion(int IDInscripcion) {
+        this.IDInscripcion = IDInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materias getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(Materias materias) {
+        this.materias = materias;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripcion{" + "IDInscripcion=" + IDInscripcion + ", alumno=" + alumno + ", materias=" + materias + ", nota=" + nota + '}';
+    }
+    
+    
+    
+}
