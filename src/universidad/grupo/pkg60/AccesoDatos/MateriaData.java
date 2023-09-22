da5fb4a6eaa504211a14477240c35dd2743a14c0
Maitena <maitenamoragues@outlook.es>
2023-09-22 20:09:19 -0300

    Intento 05555

--- src/universidad/grupo/pkg60/AccesoDatos/MateriaData.java
+++ src/universidad/grupo/pkg60/AccesoDatos/MateriaData.java
@@ -16,11 +16,71 @@ public class MateriaData {
     
     
     private Connection con = null;
+    
 
     public MateriaData() {
+    
+  
+  public class MateriaData {
+    // Atributos de la clase
+    private String Numero;
+    private String Anio;
+    
+
+    // Constructor de la clase
+    public AccesoMateriaData(String Numero, String Anio) {
+        this.Numero = Numero;
+        this.Anio = Anio;
+        
+    }
+
+    // Métodos para acceder a los datos de inscripción
+    public void guardarInscripcionData() {
+        // Implementación para guardar los datos de inscripción
+    }
+
+    public void eliminarInscripcionData() {
+        // Implementación para eliminar los datos de inscripción
+    }
+
+    public void modificarInscripcionData() {
+        // Implementación para modificar los datos de inscripción
+    }
+
+    public void obtenerInscripcionData() {
+        // Implementación para obtener los datos de inscripción
+    }
+
+    public void filtrarInscripcionData() {
+        // Implementación para filtrar los datos de inscripción
+    }
+
+    public List<InscripcionData> obtenerInscripciones() {
+        // Implementación para obtener la lista de inscripciones
+        return new ArrayList<InscripcionData>();
+    }
+
+    // Métodos para acceder a los datos de material
+    public void guardarMaterialData() {
+        // Implementación para guardar los datos de material
+    }
+
+    public void eliminarMaterialData() {
+        // Implementación para eliminar los datos de material
+    }
+
+    public void modificarMaterialData() {
+        // Implementación para modificar los datos de material
+    }
+
+    public List<MaterialData> obtenerMateriales() {
+        // Implementación para obtener la lista de materiales
+        return new ArrayList<MaterialData>();
+    }
+}
+
+                
+    
     }
     
-    
-    
-    
-}
+ }
